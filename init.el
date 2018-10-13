d037f7c9452d1b8acf15c03cf89c052e7287f9f9
Matheus Artur Cabral Costa <macc@ic.ufal.br>
2018-10-13 13:00:41 -0300

    xrandr modules

--- init.el
+++ init.el
@@ -42,4 +42,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 97 :width normal :foundry "xos4" :family "xos4 Terminus")))))
+ '(default ((t (:inherit nil :stipple nil :background "#292b2e" :foreground "#b2b2b2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "xos4" :family "xos4 Terminus")))))
