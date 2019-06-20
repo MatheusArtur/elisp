5837dbbbe9c97e2a6041d3e40552138affcbb76e
Matheus Artur Cabral Costa <macc@ic.ufal.br>
2019-06-20 01:04:57 -0300

    fixed init

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
  '(custom-enabled-themes (quote (spacemacs-dark)))
  '(custom-safe-themes
    (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
